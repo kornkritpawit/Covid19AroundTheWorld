2adca52b7a02ccc723741cd8c10a0e4b668c14a6
KasidisGit <kasidis02@gmail.com>
2020-11-26 13:31:22 +0700

    create application api

--- app.py
+++ app.py
@@ -1,12 +1,33 @@
-from flask import Flask
+import sys
+import os
 
-app = Flask(__name__)
+from config import OPENAPI_AUTOGEN_DIR
 
+if not os.path.exists(OPENAPI_AUTOGEN_DIR):
+    print("Folder '{}' not found.  "
+          "Execute openapi-generator to generate it, "
+          "e.g.,".format(OPENAPI_AUTOGEN_DIR))
+    print()
+    print("  "
+          "java -jar openapi-generator-cli-4.3.1.jar generate \\\n"
+          "     -i openapi/covid-api.yaml \\\n"
+          "     -o {} -g python-flask".format(OPENAPI_AUTOGEN_DIR))
+    print()
+    exit(1)
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+sys.path.append(OPENAPI_AUTOGEN_DIR)
 
+try:
+    import connexion
+except ModuleNotFoundError:
+    print("Please install all required packages by running:"
+          " pip install -r requirements.txt")
+    exit(1)
 
-if __name__ == '__main__':
-    app.run()
+from openapi_server import encoder
+
+app = connexion.App(__name__,)
+app.app.json_encoder = encoder.JSONEncoder
+app.add_api('openapi/covid-api.yaml')
+app.run(port=8080,debug=True)
+
