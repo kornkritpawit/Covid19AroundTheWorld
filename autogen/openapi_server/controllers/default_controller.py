fb6b9b0ba7bf71674a3bfd7765edeb9479eb578f
Vichyawat Nakarugsa <prince_vichyawat@windowslive.com>
2020-11-26 15:37:22 +0700

    Add get_covid_by_date_and_country API

--- autogen/openapi_server/controllers/default_controller.py
+++ autogen/openapi_server/controllers/default_controller.py
@@ -1,9 +1,25 @@
 import connexion
 import six
 
+from openapi_server.models.covid_case import CovidCase  # noqa: E501
 from openapi_server.models.currency import Currency  # noqa: E501
 from openapi_server.models.symbols import Symbols  # noqa: E501
 from openapi_server import util
+
+
+def controller_get_covid_by_date_and_country(country, date):  # noqa: E501
+    """Returns complete details of the specified covid all time
+
+     # noqa: E501
+
+    :param country: 
+    :type country: str
+    :param date: 
+    :type date: str
+
+    :rtype: CovidCase
+    """
+    return 'do some magic!'
 
 
 def controller_get_world_currency_rates():  # noqa: E501
