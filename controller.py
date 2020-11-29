84f97d42d21e4569cacf6931019d0555b3d3f613
Vichyawat Nakarugsa <prince_vichyawat@windowslive.com>
2020-11-29 19:08:25 +0700

    Receive Weather API

--- controller.py
+++ controller.py
@@ -39,3 +39,20 @@ def get_covid_by_date_and_country(date, country):
     confirmed = data["today_confirmed"]
     deaths = data["today_deaths"]
     return models.CovidCase(country_query, confirmed, deaths)
+
+
+def get_weather_currently_from_one_location(city_name, unit='metric'):
+    """City name can be country. Unit choose between 'metric' and 'imperial'"""
+    response = requests.get(
+        "http://api.openweathermap.org/data/2.5/weather?q=" + city_name + "&appid=9a02e2dc4526ef064473d026b12125e2"
+                                                                          "&units=" + unit)
+    name = response.json()["name"]
+    country = response.json()["sys"]["country"]
+    data = response.json()["main"]
+    temp = data["temp"]
+    feels_like = data["feels_like"]
+    temp_min = data["temp_min"]
+    temp_max = data["temp_max"]
+    pressure = data["pressure"]
+    humidity = data["humidity"]
+    return models.Weather(name, country, temp, feels_like, temp_min, temp_max, pressure, humidity)
