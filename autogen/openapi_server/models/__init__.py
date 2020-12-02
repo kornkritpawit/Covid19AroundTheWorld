39f577d1caa4d2edb708c98c47acc55fd9019839
SukritaEarn <sukrita.ki@ku.th>
2020-12-02 13:32:40 +0700

    edit controller

--- autogen/openapi_server/models/__init__.py
+++
@@ -1,7 +0,0 @@
-# coding: utf-8
-
-# flake8: noqa
-from __future__ import absolute_import
-# import models into model package
-from openapi_server.models.currency import Currency
-from openapi_server.models.symbols import Symbols
