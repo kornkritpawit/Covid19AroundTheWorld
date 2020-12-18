ee43bc1c70c21ac6d4e10a31772589e87e9a2d57
Vichyawat Nakarugsa <prince_vichyawat@windowslive.com>
2020-12-18 17:21:18 +0700

    Update map.js

--- html/Map.js
+++ html/Map.js
@@ -4,6 +4,12 @@ function unpack(rows, key) {
     });
 }
 
+function unpackTotalCases(rows, key1, key2, key3) {
+    return rows.map(function (row) {
+        return "Total Cases: " + row[key1] + "<br>" + "Total Death: " + row[key2] + "<br>" + "Total Recovered: " + row[key3];
+    });
+}
+
 
 async function getCovidCase() {
     const resp = await fetch('http://localhost:3000/graphql', {
@@ -18,21 +24,24 @@ async function getCovidCase() {
                 covid19SituationSummaryAllCountry {
                     countryName
                     totalCases
+                    totalDeathCases
+                    totalRecoveredCases
                 }
-            }`
+            }
+            `
         })
     });
-    console.log(resp)
+    // console.log(resp)
     const json = await resp.json();
-    console.log(json)
+    // console.log(json)
     let country = json.data.covid19SituationSummaryAllCountry;
-    console.log(country)
     const data = [{
         type: 'choropleth',
         locationmode: 'country names',
         locations: unpack(country, 'countryName'),
         z: unpack(country, 'totalCases'),
-        text: unpack(country, 'countryName'),
+        text: unpackTotalCases(country, 'totalCases', 'totalDeathCases', 'totalRecoveredCases'),
+
         colorscale: [
             [0,'rgb(5, 10, 172)'],[0.3,'rgb(40, 60, 190)'],
             [0.5,'rgb(70, 100, 245)'], [0.7,'rgb(90, 120, 245)'],
@@ -42,7 +51,7 @@ async function getCovidCase() {
     }];
 
     const layout = {
-        title: 'Hover cursor over the map to see all-time infected people by country.',
+        title: 'Hover cursor over the country to see the information.',
         geo: {
             projection: {
                 type: 'robinson'
