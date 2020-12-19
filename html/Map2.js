9c4683136ff06a6d589768ae8afdb1f160f91e41
Vichyawat Nakarugsa <prince_vichyawat@windowslive.com>
2020-12-19 17:03:55 +0700

    Adjust bubble map value to display

--- html/Map2.js
+++ html/Map2.js
@@ -1,13 +1,27 @@
 function intoArray(data) {
     const populationArray = [];
     const totalCasesArray = [];
+    const textArray = [];
+    const colorArray = [];
 
     for (let i = 0; i < data.length; i++) {
         populationArray.push(data[i].population);
         totalCasesArray.push(data[i].totalCases);
+        textArray.push(data[i].countryName + "<br>"
+            + "Total Cases: " +  data[i].totalCases + "<br>"
+            + "Population: " + data[i].population + "<br>"
+            + "Cases per 1 million: " + Math.round(data[i].totalCases / data[i].population * 1000000)
+        );
+        if ((data[i].totalCases / data[i].population) < 0.001) {
+            colorArray.push('rgb(44, 160, 101)');
+        } else if ((data[i].totalCases / data[i].population) < 0.01) {
+            colorArray.push('rgb(255, 144, 14)');
+        } else {
+            colorArray.push('rgb(255, 65, 54)');
+        }
     }
 
-    return [populationArray, totalCasesArray]
+    return [populationArray, totalCasesArray, textArray, colorArray]
 }
 
 async function getCovidCase() {
@@ -34,21 +48,25 @@ async function getCovidCase() {
 
     const array = intoArray(data);
     const populationArray = array[0],
-        totalCasesArray = array[1];
+        totalCasesArray = array[1],
+        countryNameArray = array[2],
+        colorArray = array[3];
 
     const trace1 = {
         x: populationArray,
         y: totalCasesArray,
+        text: countryNameArray,
         mode: 'markers',
         marker: {
-            size: Array(populationArray.length).fill(20)
+            size: Array(populationArray.length).fill(20),
+            color: colorArray
         }
     };
 
     const plotData = [trace1];
 
     const layout = {
-        title: 'Marker Size',
+        title: 'Infected and Population in each country',
         showlegend: false,
         height: 600,
         width: 900
